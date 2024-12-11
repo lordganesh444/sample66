c23fdb86b43bc6cf2da92a22c3e2cfa5de5916c5
shivani <shivashivani068@gmail.com>
2024-12-11 21:05:16 +0530

    second

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class PrimeNumbers {
     public static void main(String[] args) {
-        System.out.println("Prime numbers from 1 to 100:");
+        System.out.println("Prime numberss from 1 to 100:");
         for (int num = 1; num <= 100; num++) {
             if (isPrime(num)) {
                 System.out.print(num + " ");
