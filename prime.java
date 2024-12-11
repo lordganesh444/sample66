8e0c757a7a5b278900529e6c00792fc5a84dc3dd
shivani <shivashivani068@gmail.com>
2024-12-11 21:03:49 +0530

    first

---
+++ prime.java
@@ -0,0 +1,23 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100:");
+        for (int num = 1; num <= 100; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false; // Numbers less than or equal to 1 are not prime
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // Not prime if divisible by any number other than 1 and itself
+            }
+        }
+        return true; // Prime if no divisors are found
+    }
+}
